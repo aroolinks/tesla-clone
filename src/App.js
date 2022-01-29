fe6b3d8f540d49edad553bcffb4d5fb92664877d
HAROON KIANI <you@example.com>
2022-01-29 18:50:19 +0500

    second mes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1> Tesla-Clone</h1>
+        <p> This is firs Git up</p>
       </header>
     </div>
   );
