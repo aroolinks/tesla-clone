d3927d157f4f4b7f4fa258ae5d07c20ffce168bd
aroolinks <aroolinks@gmail.com>
2022-02-07 15:55:42 +0500

    hello world

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1> Tesla-Clone</h1>
-        <p> This is firs Git up</p>
+        <p> This is firs Git Hello Awais</p>
       </header>
     </div>
   );
