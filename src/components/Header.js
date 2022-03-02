039f7d8ee48b087951e37d909a60410d370b85fb
aroolinks <aroolinks@gmail.com>
2022-03-02 16:48:12 +0500

    final changes and completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from '../features/car/carSlice';
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
+  console.log(cars);
   return (
     <Container>
-      <a href="#">
+      <a href='#'>
         <img src='/images/logo.svg' alt='logo' />
       </a>
       <Menu>
-        <a href="#"> Model S</a>
-        <a href='#'> Model 3</a>
-        <a href='#'> Model X</a>
-        <a href='#'> Model Y</a>
+        {cars &&
+          cars.map((car, index) => (
+            <a href='#' key={index}>
+              {car}
+            </a>
+          ))}
       </Menu>
       <RightMenu>
         <a href='#'> Shop </a>
         <a href='#'> Tesla Account </a>
+        <CustomMenu onClick={() => setBurgerStatus(true)} />
       </RightMenu>
-      <CustomMenu></CustomMenu>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrapper>
+
+        {cars &&
+          cars.map((car, index) => (
+            <li key={index}>
+              {' '}
+              <a href='#'>{car}</a>
+            </li>
+          ))}
+        <li>
+          {' '}
+          <a href='#'> Shop </a>
+        </li>
+      </BurgerNav>
     </Container>
   );
 }
@@ -30,10 +55,12 @@ const Container = styled.div`
   position: fixed;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 20px;
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `;
 const Menu = styled.div`
   display: flex;
@@ -47,8 +74,13 @@ const Menu = styled.div`
     padding: 0 10px;
     flex-wrap: nowrap;
   }
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
   a {
     font-weight: 600;
     text-transformation: uppercase;
@@ -57,11 +89,44 @@ const RightMenu = styled.div`
 `;
 
 const CustomMenu = styled(MenuIcon)`
-  display: flex;
-  align-items: center;
+  cursor: pointer;
   a {
     font-weight: 600;
     text-transformation: uppercase;
     padding: 0 10px;
   }
+`;
+const BurgerNav = styled.div`
+  top: 00;
+  bottom: 0;
+  position: fixed;
+  background: white;
+  right: 0;
+  width: 300px !important;
+  display: block;
+  z-index: 9999;
+  overflow: hidden;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  text-align: start;
+  transform: ${props => (props.show ? 'translateX(0)' : 'translateX(100%)')};
+  transition: transform 0.2s;
+  li {
+    padding: 15px 0px;
+    border-bottom: 1px solid #eee;
+  }
+  a {
+    font-weight: 600;
+    cursor: pointer;
+  }
+`;
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
